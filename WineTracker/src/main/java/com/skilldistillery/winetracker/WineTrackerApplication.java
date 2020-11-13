e99ded36678214f5369b64f1f198854650339f19
Jourdan Rentschler <jrentschler3@gmail.com>
2020-11-13 16:41:53 -0700

    Stub Boot project with dependency

---
+++ WineTracker/src/main/java/com/skilldistillery/winetracker/WineTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.winetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WineTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WineTrackerApplication.class, args);
+	}
+
+}
---
+++ WineTracker/src/test/java/com/skilldistillery/winetracker/WineTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.winetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WineTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
