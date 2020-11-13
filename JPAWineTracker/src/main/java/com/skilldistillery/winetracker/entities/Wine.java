d9a5345c285907cf5c14dd2156d564637ac2696d
Jourdan Rentschler <jrentschler3@gmail.com>
2020-11-13 16:11:03 -0700

    Validate JPA/Hibernate config

--- JPAWineTracker/src/main/java/com/skilldistillery/winetracker/entities/Wine.java
+++ JPAWineTracker/src/main/java/com/skilldistillery/winetracker/entities/Wine.java
@@ -1,10 +1,67 @@
 package com.skilldistillery.winetracker.entities;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 
 
 @Entity 
 public class Wine {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String name;
+	
+	public Wine() {
+		super();
+	}
+	public Wine(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Wine [id=").append(id).append(", name=").append(name).append("]");
+		return builder.toString();
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Wine other = (Wine) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+	
+	
 
 }
--- JPAWineTracker/src/test/java/com/skilldistillery/winetracker/entities/WineTest.java
+++ JPAWineTracker/src/test/java/com/skilldistillery/winetracker/entities/WineTest.java
@@ -1,6 +1,11 @@
 package com.skilldistillery.winetracker.entities;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
@@ -10,25 +15,36 @@ import org.junit.jupiter.api.Test;
 
 class WineTest {
 
+	private static EntityManagerFactory emf; 
+	private EntityManager em;
+	private Wine wine;
+	
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("WineTrackerPU");
 	}
 
 	@AfterAll
 	static void tearDownAfterClass() throws Exception {
+		emf.close();
 	}
 
 	@BeforeEach
 	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		wine = em.find(Wine.class, 1);
 	}
 
 	@AfterEach
 	void tearDown() throws Exception {
+		em.close();
+		wine = null;
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void test_Wine_Entity_mapping() {
+		assertNotNull(wine);
+		assertEquals("LaCrema Pinot Noir", wine.getName());
 	}
 
 }
