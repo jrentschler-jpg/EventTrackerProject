9e1db19ddf9a8c89e3d79e9bf480baab6c59acb4
Jourdan Rentschler <jrentschler3@gmail.com>
2020-11-20 17:36:45 -0700

    part 2 of project started created js and html.

---
+++ WineTracker/src/main/resources/static/js/eventTracker.js
@@ -0,0 +1,13 @@
+window.addEventListener("load", function () {
+  console.log("Script loaded");
+  init();
+});
+function init() {
+  console.log("in init()");
+  //TODO: set up event listeners for buttons, etc.
+}
+
+//xhr.open('PUT', api/winetracker/wineId);
+// xhr.send(JSON.stringify(updatedWine))
+
+// xhr.open('DELETE', api/winetracker/' + wineId);
